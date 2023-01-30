77121628529417768fb0159a1209e34e12fa2d9f
NatanRei <nreischmura@gmail.com>
2023-01-30 19:32:15 -0300

    feat: using native Image component from next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,14 @@ import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
 import LogoImg from '../assets/ntn-blog.png'
 import { Container, Header } from "../styles/pages/app";
+import Image from "next/image";
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (<Container>
     <Header>
-      <img src={LogoImg.src} alt="Logo NTN Store" />
+      <Image src={LogoImg} width={70} alt="Logo NTN Store" />
     </Header>
     <Component {...pageProps} />
   </Container>
