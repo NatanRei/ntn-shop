03bc35ec4ee28c1026ef4a224c4a5d0c7e94983c
NatanRei <nreischmura@gmail.com>
2023-02-01 19:45:20 -0300

    feat: connection with strapi, and put data on the screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,73 @@
 import Image from "next/image"
 import { HomeContainer, Product } from "../styles/pages/home"
 
-import shirt1 from '../assets/shirt1.png'
-import shirt2 from '../assets/shirt2.png'
-import shirt3 from '../assets/shirt3.png'
+import { useKeenSlider } from "keen-slider/react"
+
+import 'keen-slider/keen-slider.min.css'
+import { stripe } from "../lib/stripe"
+import { GetStaticProps } from "next"
+import Stripe from "stripe"
+import Link from "next/link"
 
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    id: string,
+    name: string,
+    imageUrl: string,
+    price: number
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
+
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
-        <Image src={shirt1} width={520} height={400} alt="" />
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {products.map(product => (
+        <Link href={`/products/${product.id}`} key={product.id}>
+        <Product className="keen-slider__slide" >
+        <Image src={product.imageUrl} width={520} height={400} alt="" />
         <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
+          <strong>{product.name}</strong>
+          <span>{product.price}</span>
         </footer>
       </Product>
-      <Product>
-        <Image src={shirt3} width={520} height={400} alt="" />
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-      <Product>
-        <Image src={shirt2} width={520} height={400} alt="" />
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
+      </Link>
+      ))}
+      
     </HomeContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  });
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price 
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount / 100)
+
+
+    }
+  })
+  return {
+    props: {
+      products: products
+    },
+    revalidate: 60 * 60 * 2
+  }
 }
--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,11 +1,19 @@
 import { useRouter } from "next/router";
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/products";
 
 
 export default function Products() {
     const { query } = useRouter();
     console.log(query);
     return (
-        
-        <h1>Products</h1>
+        <ProductContainer>
+            <ImageContainer></ImageContainer>
+            <ProductDetails>
+                <h1>shirt one</h1>
+                <span>R$ 79,90</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quas iste incidunt neque facere odio, officia consequuntur numquam quo repellendus hic eum, ut suscipit architecto eaque deleniti rem illum modi!</p>
+                <button>Comprar agora</button>
+            </ProductDetails>
+        </ProductContainer>
     );
 }
