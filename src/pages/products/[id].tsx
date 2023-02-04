92e74cd83b24913ccc1b97e8215c45d5af8239d7
NatanRei <nreischmura@gmail.com>
2023-02-04 12:57:02 -0300

    feat: products page and styles

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,19 +1,91 @@
-import { useRouter } from "next/router";
+import axios from "axios";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import Stripe from "stripe";
+import { stripe } from "../../lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/products";
 
+interface ProductProps {
+    product: {
+      id: string,
+      name: string,
+      description: string,
+      imageUrl: string,
+      price: number,
+      defaultPriceId: string
+    }
+  }
 
-export default function Products() {
-    const { query } = useRouter();
-    console.log(query);
+export default function Products({ product }: ProductProps) {
+    const [ isCreatingCheckoutSession, setIsCreatingCheckoutSession ] = useState(false)
+
+    async function handleBuyProduct() {
+        try {
+            setIsCreatingCheckoutSession(true)
+            const response = await
+             axios.post('/api/checkout',{
+                priceId: product.defaultPriceId
+            })
+            const { checkoutUrl } = response.data;
+
+            window.location.href = checkoutUrl;
+        } catch (error) {
+            setIsCreatingCheckoutSession(false)
+
+        }
+    }
+
     return (
-        <ProductContainer>
-            <ImageContainer></ImageContainer>
-            <ProductDetails>
-                <h1>shirt one</h1>
-                <span>R$ 79,90</span>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quas iste incidunt neque facere odio, officia consequuntur numquam quo repellendus hic eum, ut suscipit architecto eaque deleniti rem illum modi!</p>
-                <button>Comprar agora</button>
-            </ProductDetails>
-        </ProductContainer>
+        <>
+            <Head><title>{product.name} | NTN Shop</title></Head>
+
+            <ProductContainer>
+                <ImageContainer>
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
+                </ImageContainer>
+                <ProductDetails>
+                    <h1>{product.name}</h1>
+                    <span>{product.price}</span>
+                    <p>{product.description}</p>
+                    <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>Comprar agora</button>
+                </ProductDetails>
+            </ProductContainer>
+        </>
     );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [],
+        fallback: 'blocking'
+    }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+    const productId = params.id;
+
+    const product = await stripe.products.retrieve(productId, {
+        expand: ['default_price']
+    });
+
+    const price = product.default_price as Stripe.Price
+    console.log(product)
+    return {
+        props: {
+            product: {
+                id: product.id,
+                name: product.name,
+                imageUrl: product.images[0],
+                description: product.description,
+                price: new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                }).format(price.unit_amount / 100),
+                defaultPriceId: price.id          
+              }
+        },
+        revalidate: 60 * 60
+    }
 }
